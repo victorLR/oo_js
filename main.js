7d886893e79c71dec275fc67403ade69797147c8
VictorRL <victoraffonso1997@gmail.com>
2024-11-29 17:23:17 -0300

    adicionando chamada da moto

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function Veiculo(marca, modelo) {
 }
 
 function Carro(marca, modelo, portas) {
-  Veiculo.call(this, marca, modelo); // Chama o construtor da classe base
+  Veiculo.call(this, marca, modelo); 
   this.portas = portas;
 
   this.detalhes = function () {
@@ -16,19 +16,21 @@ function Carro(marca, modelo, portas) {
   };
 }
 
-function Moto(marca, modelo, portas) {
-  Veiculo.call(this, marca, modelo); // Chama o construtor da classe base
-  this.portas = portas;
+function Moto(marca, modelo, cilindradas) {
+  Veiculo.call(this, marca, modelo);
+  this.cilindradas = cilindradas;
 
   this.detalhes = function () {
-    return `${this.marca} ${this.modelo} é uma moto com ${this.portas} portas.`;
+    return `${this.marca} ${this.modelo} é uma moto com ${this.cilindradas} Cilindradas.`;
   };
 }
 
 const carro1 = new Carro("Toyota", "Corolla", 4);
 const carro2 = new Carro("Volkswagen", "Golf", 2);
 const carro3 = new Carro("Volkswagen", "Gol", 2);
+const moto = new Moto("YAMAHA","Lander",250)
 
 console.log(carro1.detalhes());
 console.log(carro2.detalhes());
 console.log(carro3.detalhes());
+console.log(moto.detalhes());
